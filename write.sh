e2cd3d0e02c4ff7aa1ab1452cb48d61da21bd8e0
Pascal Bleser <pascal.bleser@opensuse.org>
2010-04-20 23:28:43 +0200

    run rawdog with TZ=UTC

--- write.sh
+++ write.sh
@@ -22,6 +22,7 @@ if [ -n "$render_all" -o -n "$render_global" ]; then
     info "* global"
     mkdir -p website/global
     LC_ALL=en_US.utf8 \
+    TZ=UTC \
     ./rawdog -d planetsuse/ \
         --output-dir=website/global \
         --write
@@ -39,6 +40,7 @@ for lc in en_US de_DE es_ES pl_PL pt_PT ja_JP; do
         *) lcu="${lc}.utf8" ;;
     esac
     LC_ALL="$lcu" \
+    TZ=UTC \
     ./rawdog -d planetsuse/ \
         --lang="$lang" \
         --output-dir=website/"$lang" \
