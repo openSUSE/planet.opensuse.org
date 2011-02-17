2f229a3d3f531c535bcadd14310f62baa6518846
Pascal Bleser <pascal.bleser@opensuse.org>
2011-02-17 07:36:45 +0100

    suspend suseroot feed, gives an error

--- planetsuse/stats.pl
+++ planetsuse/stats.pl
@@ -2,6 +2,11 @@
 # vim: set ts=4 sw=4 et:
 use strict;
 use warnings;
+my $has_plot = 0;
+eval {
+    use Imager::Graph::Pie;
+    $has_plot = 1;
+};
 
 my %names = ();
 my %feeds = ();
@@ -50,4 +55,43 @@ my %bylang = ();
 print scalar(@blocks), " blocks", "\n";
 print scalar(keys(%names)), " bloggers", "\n";
 print scalar(keys(%feeds)), " feeds", "\n";
-print "By language: ", join(", ", map { $_."=".$bylang{$_} } sort { $bylang{$b} - $bylang{$a} } keys %bylang), "\n";
+print "By language:", "\n", join("\n", map { $_."=".$bylang{$_} } sort { $bylang{$b} - $bylang{$a} } keys %bylang), "\n";
+
+if ($has_plot) {
+    my $data = [];
+    my $labels = [];
+    foreach my $lang (sort { $bylang{$b} - $bylang{$a} } keys %bylang) {
+        push(@$data, $bylang{$lang});
+        push(@$labels, $lang);
+    }
+    my $tfont = Imager::Font->new(file => '/usr/share/fonts/truetype/DejaVuSansCondensed-Bold.ttf');
+    my $lfont = Imager::Font->new(file => '/usr/share/fonts/truetype/DejaVuSansCondensed.ttf');
+    my $pie = Imager::Graph::Pie->new;
+    my $img = $pie->draw(
+        data => $data,
+        labels => $labels,
+        font => $lfont,
+        width => 400,
+        height => 300,
+        style => 'primary',
+        back => 'FFFFFF',
+        legend => {
+            font => $lfont,
+            fill => 'EEEEEE',
+        },
+        title => {
+            text => 'Planet openSUSE by language',
+            size => 18,
+            font => $tfont,
+            color => '000000',
+            halign => 'center',
+            valign => 'top',
+        },
+        features => ['labelspc', 'nolegend', 'nodropshadow', 'nooutline'],
+    ) or die $pie->error;
+    $img->write(file => 'stats.png');
+} else {
+    print "\n", "Install Imager::Graph to generate fancy graphs", "\n";
+}
+
+
