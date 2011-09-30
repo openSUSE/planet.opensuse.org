775d6d68154ba73557a858d006f9767cc11ef219
Pascal Bleser <pascal.bleser@opensuse.org>
2011-09-30 15:15:22 +0200

    add git pull before rendering in cron job

--- cron.sh
+++ cron.sh
@@ -4,14 +4,18 @@ basedir="${0%/*}"
 [ -n "$basedir" -a "$basedir" != "." ] && cd "$basedir"
 
 [ -e ./NOCRON ] && exit 0
+[ -e ./NOPULL ] || {
+    git pull --quiet
+}
 
 LOGFILE="$PWD/website/log.txt"
 
 {
-    echo -n "START: "
+    REV=$(git rev-parse HEAD)
+    echo -n "========== START ($REV): "
     /bin/date -R
-    ./update.sh
-    ./write.sh
-    echo -n "END: "
+    ./update.sh 2>&1
+    ./write.sh 2>&1
+    echo -n "========== END ($REV): "
     /bin/date -R
 } >"$LOGFILE" 2>&1
